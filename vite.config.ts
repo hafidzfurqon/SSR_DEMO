98ec28ce0323e990176b2baab264052c74fe4236
hafidzfurqon <hafidzfurqonyet@gmail.com>
2025-05-09 11:24:49 +0700

    feat : launch deployments

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import vercel from 'vite-plugin-vercel'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    vercel()
-  ]
-})
+  plugins: [react(), vercel()],
+  ssr: {
+    noExternal: ["react-helmet-async"], // jika perlu
+  },
+});
